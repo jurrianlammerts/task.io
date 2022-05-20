303fbab1cc1120eea2d4fcd8dd611b74b3b78ce8
Jurrian <jurrianlammerts@hotmail.com>
2022-05-20 15:56:43 +0200

    Setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
-module.exports = function(api) {
-  api.cache(true);
+const path = require("path")
+module.exports = function (api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".js",
+            ".ts",
+            ".tsx",
+            ".json",
+            ".png",
+            ".otf",
+            ".woff2",
+          ],
+          alias: {
+            "@assets": path.resolve(__dirname, "./assets"),
+            "@components": path.resolve(__dirname, "./components"),
+            "@screens": path.resolve(__dirname, "./screens"),
+            "@navigation": path.resolve(__dirname, "./navigation"),
+            "@lib": path.resolve(__dirname, "./lib"),
+            "@config": path.resolve(__dirname, "./config"),
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+  }
+}
